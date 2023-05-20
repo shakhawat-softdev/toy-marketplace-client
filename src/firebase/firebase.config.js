fef550226e4222562362ffd528015e0dc2059218
Shakhawat <syedarafatcse@gmail.com>
2023-05-20 19:55:10 +0600

    firebase cofig api keys are hided by environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyCsAZ5apCD75EBNwq9Hyo-kDuZ8ckyUYiM",
-   authDomain: "toy-marketplace-client-s-60275.firebaseapp.com",
-   projectId: "toy-marketplace-client-s-60275",
-   storageBucket: "toy-marketplace-client-s-60275.appspot.com",
-   messagingSenderId: "827352479542",
-   appId: "1:827352479542:web:940975b4be63dfd2ada597"
+   apiKey: import.meta.env.VITE_APIKEY,
+   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+   projectId: import.meta.env.VITE_PROJECTID,
+   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+   appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
